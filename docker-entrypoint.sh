71fee287e9dc4be385f640620129e362e67b6a38
Delevedove <42685403+rpx01@users.noreply.github.com>
2025-07-21 16:13:16 -0300

    Revert "Add migrations and entrypoint fix"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# Exporta a variável de ambiente para que o comando flask funcione
-export FLASK_APP=src/main.py
+echo "Waiting for database..."
+# TODO: Add wait-for-db logic if necessary
 
 echo "Applying database migrations..."
-# Aplica as migrações do banco de dados
-flask db upgrade
+flask --app src.main db upgrade
 
 echo "Starting Gunicorn server..."
-# Inicia o servidor da aplicação
 exec gunicorn --bind 0.0.0.0:8080 "src.main:app"

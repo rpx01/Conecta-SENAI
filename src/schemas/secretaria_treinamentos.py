40ccfa9dab3e088ea9de41929c309007993c06c2
Delevedove <42685403+rpx01@users.noreply.github.com>
2025-09-12 12:57:51 -0300

    Revert "feat: add turma email notifications"

--- src/schemas/secretaria_treinamentos.py
+++ src/schemas/secretaria_treinamentos.py
@@ -3,6 +3,5 @@ from marshmallow import Schema, fields
 
 class SecretariaTreinamentosSchema(Schema):
     id = fields.Int(dump_only=True)
-    nome = fields.Str(required=False, allow_none=True)
+    nome = fields.Str(required=True)
     email = fields.Email(required=True)
-    ativo = fields.Bool(dump_only=True)

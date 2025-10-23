9440f43c9ca5288cbfeca7884efc073578c276bc
Delevedove <42685403+rpx01@users.noreply.github.com>
2025-10-23 08:29:22 -0300

    Revert "Add suporte de TI module with backend and frontend"

--- src/routes/suporte_ti/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Blueprints do módulo de suporte de TI."""
-from .publico import suporte_ti_public_bp  # noqa: F401
-from .admin import suporte_ti_admin_bp  # noqa: F401
-
-__all__ = [
-    "suporte_ti_public_bp",
-    "suporte_ti_admin_bp",
-]
